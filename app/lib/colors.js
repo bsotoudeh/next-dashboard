11a9adf3b530cad1e227ac38a7d21bfe99dd9ade
bsotoudeh <bsotoudehnik@dataguard.de>
2025-11-27 17:26:12 +0330

    feat: add color library

---
+++ app/lib/colors.js
@@ -0,0 +1,12 @@
+export const colorMap = {
+  "Cloudy White": "bg-gray-200 text-gray-800",
+  "White": "bg-gray-100 text-gray-800",
+  "Blue": "bg-blue-500 text-white",
+  "Purple": "bg-purple-500 text-white",
+  "Brown": "bg-amber-700 text-white",
+  "Red": "bg-red-600 text-white",
+  "Elderberry": "bg-purple-800 text-white",
+};
+
+export const defaultColor = "bg-gray-300 text-gray-900";
+
